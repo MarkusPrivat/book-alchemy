36b7a12bf5a35c7ef43aa3c0c2d6d9da95b95418
Markus <25428460+MarkusPrivat@users.noreply.github.com>
2026-03-09 23:27:14 +0100

    create app.route('/add_author')

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from datetime import date
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect
 
@@ -14,6 +15,40 @@ DATABASE_PATH = PROJECT_ROOT / "data/library.sqlite"
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DATABASE_PATH.as_posix()}"
 db.init_app(app)
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'GET':
+        return render_template('add_author.html')
+    if request.method == 'POST':
+        name_form = request.form.get('name', '').strip()
+        birthdate_form = request.form.get('birthdate', '')
+        date_of_death_form = request.form.get('date_of_death', '')
+
+        if not name_form:
+            return "Name cannot be empty!", 400
+        if not birthdate_form:
+            return "Author must have a birthdate!", 400
+
+        try:
+            birth_date_obj = date.fromisoformat(birthdate_form)
+            death_date_obj = None
+            if date_of_death_form:
+                death_date_obj = date.fromisoformat(date_of_death_form)
+            author = Author(
+                name=name_form,
+                birth_date=birth_date_obj,
+                date_of_death=death_date_obj
+            )
+
+            db.session.add(author)
+            db.session.commit()
+
+            return f"Author {name_form} successfully added!", 200
+
+        except ValueError:
+            return "Invalid date format received. Please use YYYY-MM-DD.", 400
 
 
 def init_db():
